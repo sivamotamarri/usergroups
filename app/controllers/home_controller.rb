50e7114b595c70351768f611dbf6cd1c689be4e2
sagarbommidi <sagar.b@imaginea.com>
2012-10-09 17:13:19 +0530

    placed the map in landing page along with some sample locations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
 
   def get_markers
   	markers = []
-  	%w(Hyderabad Chennai Banglore Mumbai Pune).each do |city|
+  	%w(Hyderabad Chennai CA Mexico China).each do |city|
   		options = Gmaps4rails.geocode(city)
   		markers << {:lat => options.first[:lat], :lng => options.first[:lng], :title => options.first[:matched_address]}
   	end
