707ff3efd57ff2ec6b35d700f1c92d7adb2eab3c
sivamotamarri <sivakrishna.motamarri@gmail.com>
2012-10-12 17:26:05 +0530

    fix redirect issue

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Admin::SessionsController < ApplicationController
 
   def new
+    reset_session
   end
 
   def create
@@ -10,10 +11,14 @@ class Admin::SessionsController < ApplicationController
       if status && user.admin?
         session[:user_id] = user.id
         redirect_to admin_chapters_url, :notice => "Logged in!"
+      else
+       flash.now.alert = "Invalid email or password"
+       render "new"
       end
-    end
-      flash.now.alert = "Invalid email or password"
-      render "new"       
+    else
+       flash.now.alert = "Invalid email or password"
+       render "new"
+    end      
   end
 
   def destroy
