812aba1357c5b7280b32c367390f422a369a7698
siva krishna <sivakrishna.m@imaginea.com>
2012-11-22 12:14:08 +0530

    added s3 storage service

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -5,9 +5,16 @@ class Announcement < ActiveRecord::Base
    attr_accessible :title, :body , :photo , :user_id 
 
 
-   has_attached_file :photo,
+#   has_attached_file :photo,
+#    :styles => { :medium => "650x438>", :thumb => "128x90>" , :mini => "60x60>" },
+#    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+#    :url => "/system/:attachment/:id/:style/:filename"
+
+  has_attached_file :photo,
     :styles => { :medium => "650x438>", :thumb => "128x90>" , :mini => "60x60>" },
-    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-    :url => "/system/:attachment/:id/:style/:filename"
+    :path => "/:attachment/:id/:style/:filename",
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/s3.yml"
+
    validates :title , :body , :presence => true
 end
